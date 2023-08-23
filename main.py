17f584c21beb9ed1d75d17cf0d9060a22ecec540
Armin Shabani <50800254+ArminShabani@users.noreply.github.com>
2023-08-23 21:29:48 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import sounddevice as sd
+import soundfile as sf
+from tkinter import *
+
+
+def voice_rec():
+    fs = 48000
+    
+    duration= 5
+    myrecording = sd.rec(int(duration * fs),samplerate = fs , channels =2)
+    
+    sd.wait()
+    
+    return sf.write('My_Audio_Recording.flac',myrecording, fs)
+
+master = Tk()
+
+Label(master, text="Voice recorder :").grid(row = 0, sticky = W , rowspan = 5)
+
+b = Button(master, text="start",command=voice_rec)
+b.grid(row = 0, column=2, columnspan=2,rowspan=2, padx=5 , pady=5)
+
+mainloop()
+
+
+
+    
